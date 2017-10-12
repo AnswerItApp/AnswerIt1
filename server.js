794cb71fd265016ce425485e8ee29b3c86a36533
AnswerIt <cool.eee1222@gmail.com>
2017-10-12 16:11:52 +0200

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
+var router = require('express').Router();
 
 var app = express();
 
@@ -14,6 +15,41 @@ app.set('port', (process.env.PORT || 4000));
 app.get('/', function(req, res){
   res.render('home');
 });
+
+router.post('/contact', function(req, res){
+  req.check('email', 'Niepoprawny email').isEmail();
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+  }
+  var transporter = nodemailer.createTransport({
+  host: "smtp.zoho.eu", // hostname
+  secureConnection: true, // use SSL
+  port: 465, // port for secure SMTP
+  auth: {
+    user: 'kontakt@monache-sklep.pl',
+    pass: 'monika20'
+  }
+});
+
+var mailOptions = {
+  from: 'Monache <kontakt@monache-sklep.pl>',
+    to: 'kontakt@monache-sklep.pl',
+    subject: 'Zapytanie',
+   text: 'Masz następującą wiadomość:  Imię: ' + req.body.name+ 'Email: '+req.body.email+ 'Treść: '+req.body.message,
+   html: '<p> Masz następującą wiadomość: </p><ul><li>Imię: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Treść: '+req.body.message+'</li></ul>',
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+   if(error) {
+    console.log(error);
+     res.redirect('/');
+  } else {
+    res.redirect('/');
+  };
+   });
+   });
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
